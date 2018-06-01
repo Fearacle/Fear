b855d351e0087282f6daab4003952bd057ae3aeb
Fearacle <260198q_>
2018-06-01 19:25:21 +0300

    lab7 truck

---
+++ src/lab7/b/truck.java
@@ -0,0 +1,15 @@
+package lab7.b;
+
+
+public class truck implements passengersauto, cargoauto {
+    @Override
+    public void translateCargo() {
+        // a lot cargo
+
+    }
+
+    @Override
+    public void translatePassengers() {
+        // no passengers
+    }
+}
