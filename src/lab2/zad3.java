e65e855ace7fb607956780fc2cbcd2384a88a805
Fearacle <260198q_>
2018-06-01 15:25:24 +0300

    лаба2 задание 3

---
+++ src/lab2/zad3.java
@@ -0,0 +1,13 @@
+package lab2;
+
+
+public class zad3 {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < 10; i++) {
+            builder.append('8');
+            System.out.println(builder);
+        }
+    }
+}
