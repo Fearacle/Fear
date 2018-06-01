6708f3703efb3c31380bac04d881d01386566bc4
Fearacle <260198q_>
2018-06-01 19:24:33 +0300

    lab7 sedan

---
+++ src/lab7/b/sedan.java
@@ -0,0 +1,14 @@
+package lab7.b;
+
+
+public class sedan implements passengersauto, cargoauto {
+    @Override
+    public void translateCargo() {
+        // no cargo
+    }
+
+    @Override
+    public void translatePassengers() {
+        // ~4 passengers
+    }
+}
