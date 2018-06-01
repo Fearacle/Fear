dacaed7c7810c4905bfefc8c683d155a141325c6
Fearacle <260198q_>
2018-06-01 19:32:55 +0300

    lab7 cow

---
+++ src/lab7/d/cow.java
@@ -0,0 +1,19 @@
+package lab7.d;
+
+
+public class cow {
+
+    public void printAll() {
+        printColor();
+        printName();
+    }
+
+    public void printColor() {
+        System.out.println("Я – белый");
+    }
+
+    public void printName() {
+        System.out.println("Я – корова");
+    }
+
+}
