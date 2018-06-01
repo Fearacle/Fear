d5de02b4291c93c707b76a3b2dd15099e0c4413a
Fearacle <260198q_>
2018-06-01 18:52:06 +0300

    lab6 horse

---
+++ src/lab6/horse.java
@@ -0,0 +1,16 @@
+package lab6;
+
+
+
+@SuppressWarnings("ALL")
+public class horse {
+    private String name;
+
+    public horse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
