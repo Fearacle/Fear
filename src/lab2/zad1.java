bd168e232e05407298cb602d2f4b95089b6a6b96
Fearacle <260198q_>
2018-06-01 15:19:35 +0300

    лаба2 задание 1

---
+++ src/lab2/zad1.java
@@ -0,0 +1,12 @@
+package lab2;
+
+
+
+public class zad1 {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < 101; i++) builder.append(i).append(' ');
+        System.out.println(builder);
+    }
+}
+
