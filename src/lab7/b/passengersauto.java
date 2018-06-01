57867511b4501f562aca1982731397e05e71de56
Fearacle <260198q_>
2018-06-01 19:23:19 +0300

    lab7 pickup

--- src/lab7/b/passengersauto.java
+++ src/lab7/b/passengersauto.java
@@ -3,6 +3,6 @@ package lab7.b;
 /**
  * 0da - 21.01.2018.
  */
-public interface PassengersAuto {
+public interface passengersauto {
     void translatePassengers();
 }
---
+++ src/lab7/b/pickup.java
@@ -0,0 +1,14 @@
+package lab7.b;
+
+
+public class pickup implements passengersauto, cargoauto {
+    @Override
+    public void translateCargo() {
+        // bare cargo
+    }
+
+    @Override
+    public void translatePassengers() {
+        // ~4 passengers
+    }
+}
